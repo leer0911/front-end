6d27bae9fde94924ee7f314d897700b3390a9754
lixinxiang <lixinxiang@xiaoyouzi.com>
2018-04-26 17:09:25 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const base = process.env.GH ? '/vuepress/' : '/';
+const basePath = './docs/';
+const arrGenerate = files => {
+  const arr = [''];
+  files.forEach(function(file) {
+    const fileName = file.replace('.md', '');
+    if (fileName !== 'README') {
+      arr.push(fileName);
+    }
+  });
+  return arr;
+};
+
+const nav = [];
+const sidebar = {};
+
+fs.readdir(basePath, function(err, files) {
+  if (err) {
+    return console.error(err);
+  }
+
+  files.forEach(function(file) {
+    if (file === '.vuepress' || file === 'README.md') {
+      return;
+    }
+
+    nav.push({
+      text: file.charAt(0).toUpperCase() + file.slice(1),
+      link: `/${file}/`
+    });
+    fs.readdir(`${basePath}${file}`, function(err, files) {
+      if (err) {
+        return console.error(err);
+      }
+      sidebar[`/${file}/`] = [
+        {
+          title: file,
+          collapsable: false,
+          children: arrGenerate(files)
+        }
+      ];
+    });
+  });
+});
+module.exports = {
+  title: '📚 FECODING',
+  description: 'FrontEnd Code & Artilce Collection By VuePress',
+  dest: 'vuepress',
+  base,
+  head: [['link', { rel: 'icon', href: `/logo.png` }]],
+  serviceWorker: true,
+  themeConfig: {
+    repo: 'leer0911/front-ends',
+    editLinks: true,
+    docsDir: 'docs',
+    nav,
+    sidebar
+  }
+};
---
+++ docs/README.md
@@ -0,0 +1,5 @@
+# FrontEnd
+
+## Todo
+
+* [x] [AMD CommonJS and ES6 Modules Usage](js/amd-commonjs-es6modules.js)
